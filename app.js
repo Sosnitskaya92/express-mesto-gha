a548050a8bff6ef867389411ef70346458d018d4
Angelina <Sosnitskaya92@mail.ru>
2022-05-04 12:34:32 +0300

    body-parser

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-    useFindAndModify: false
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// mongoose.connect('mongodb://localhost:27017/mestodb', {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//     useFindAndModify: false
+// });
 
 app.listen(PORT, () => {
   console.log(`App listener on port ${PORT}`)
-}}
+}
